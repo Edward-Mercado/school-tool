b05c9a7a74e8bc391ace4b4a98e58c7bb9a71160
Edward-Mercado <edwardmercado149@gmail.com>
2026-04-02 13:59:08 -0400

    okay all of my imports are in

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="bg-red-500">
         test lmfao
+        <div class="breadcrumbs text-sm">
+  <ul>
+    <li><a>Home</a></li>
+    <li><a>Documents</a></li>
+    <li>Add Document</li>
+  </ul>
+</div>
     </div>
 </template>
 
